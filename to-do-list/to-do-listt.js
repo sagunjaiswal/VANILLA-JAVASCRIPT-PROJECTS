55ba86d37e6466bbaa57c7d8a3320563192769b9
SAGUN JAISWAL <63302439+sagunjaiswal@users.noreply.github.com>
2020-06-21 15:40:29 +0530

    Add files via upload

---
+++ to-do-list/to-do-listt.js
@@ -0,0 +1,60 @@
+const addButton  = document.querySelector('.addButton');
+var input = document.querySelector('.input');
+const conatiner = document.querySelector('.container');
+
+class  item{
+    constructor(itemName){
+        this.createDiv(itemName);
+    }
+
+    createDiv(itemName){
+        let input = document.createElement('input');
+        input.value = itemName;
+        input.disabled = true;
+        input.classList.add('item_input');
+        input.type = "text";
+
+        let itemBox = document.createElement('div');
+        itemBox.classList.add('item');
+
+        let editButton = document.createElement('button');
+        editButton.innerHTML = "EDIT";
+        editButton.classList.add('editButton');
+
+        let removeButton = document.createElement('button');
+        removeButton.innerHTML = "REMOVE";
+        removeButton.classList.add('removeButton');
+
+        conatiner.appendChild(itemBox);
+       
+        itemBox.appendChild(input);
+        itemBox.appendChild(editButton);
+        itemBox.appendChild(removeButton);
+
+        editButton.addEventListener('click', () => this.edit(input));
+        removeButton.addEventListener('click', () => this.remove(itemBox));
+    }
+
+    edit(input){
+        input.disabled = !input.disabled;
+    }
+    remove(item){
+        conatiner.removeChild(item);
+            }
+
+}
+
+function check(){
+    if(input.value != ""){
+        new item(input.value);
+        input.value = "";
+    }
+}
+
+addButton.addEventListener('click', check);
+window.addEventListener('keydown', (e) => {
+    if(e.which == 13){
+        check();
+    }
+})
+// new item("Sportt");
